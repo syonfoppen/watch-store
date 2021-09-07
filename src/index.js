69f8eb4f849aaad57da0fb14da06dc46b16b3e39
Syon Foppen <ikbensyon@gmail.com>
2021-09-07 09:08:30 +0200

    no message

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ const port = 8080;
 
 app.use(express.json());
 
+app.use('/products', require('./routes/products'));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,2 +1,15 @@
 const express = require('express');
+const {StatusCodes} = require('http-status-codes');
+
 const router = express.Router();
+
+router.get('',(req, res ) => {
+    res.send('Dit word een lijst met producten');
+});
+
+router.post('',(req, res ) => {
+    res.
+        status(StatusCodes.CREATED).send('Hier gaan we producten toevoegen');
+});
+
+module.exports = router;
