2b9f5d455f0e3a3ed0ea7e3b642ca5c8d09ea709
Syon Foppen <ikbensyon@gmail.com>
2021-09-07 11:15:02 +0200

    no message

--- src/routes/products.js
+++ src/routes/products.js
@@ -19,7 +19,7 @@ router.get('',(req, res ) => {
 });
 
 router.post('',(req, res ) => {
-    let newProduct = new product(1, req.body.brand, req.body.model, req.body.starting_price, req.body.description, req.body.year,213,"glass, true");
+    let newProduct = new product(1, req.body.brand, req.body.model, req.body.startingPrice, req.body.description, req.body.year,req.body.diameter, req.body.material, req.body.waterResistance);
     console.log(req.body);
 
     productList.push(newProduct);
