3bff2dd0606fe59664150cab09a1dea5d8756b60
Syon Foppen <ikbensyon@gmail.com>
2021-09-08 09:01:24 +0200

    fix

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,7 @@ const {StatusCodes} = require('http-status-codes');
 const bodyParser = require('body-parser')
 
 let product = require('../modules/product');
-let productList = require("../collections/productCollection")
+let productCollection = require("../collections/productCollection")
 
 const router = express.Router();
 router.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -15,21 +15,18 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 router.get('',(req, res ) => {
-    res.send(productList);
+    res.send(productCollection);
 });
 
 router.post('',(req, res ) => {
     let newProduct = new product(1, req.body.brand, req.body.model, req.body.startingPrice, req.body.description, req.body.year,req.body.diameter, req.body.material, req.body.waterResistance);
     console.log(req.body);
 
-    productList.push(newProduct);
+    productCollection.push(newProduct);
 
 
     res.
         status(StatusCodes.CREATED).send(newProduct);
 });
 
-router.get('/watches',(req, res ) => {
-    res.send('Dit word een lijst met watches');
-});
 module.exports = router;
